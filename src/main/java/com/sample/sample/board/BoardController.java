4ad143c5c080b3ac4ded42b730d7c326dc90e712
SuperYoplait <60890985+SuperYoplait@users.noreply.github.com>
2022-02-09 15:48:41 +0900

    테스트

--- src/main/java/com/sample/sample/board/BoardController.java
+++ src/main/java/com/sample/sample/board/BoardController.java
@@ -1,28 +1,25 @@
 package com.sample.sample.board;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/test")
+@RequestMapping("/")
 public class BoardController {
     
 
     @GetMapping("/list")
-    public String HomeList(){ // 용훈
-        return "test";
+    public String HomeList(){
+        return "";
     }
 
-    //게시글 작성 및 수정
     @GetMapping("/addboard")
-    public String AddBoard(@RequestParam (required = false) Long id, Model model){
-        model.addAttribute("title", "게시글 작성");
-        return "/board/boardaddForm";
+    public String AddBoard(){
+        return "";
     }
 
+    //테스트
     
 
     @GetMapping("/detail-board")
