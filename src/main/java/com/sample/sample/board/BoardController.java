30dfc2c0b49fd4f94eb46857cdf41c0badde59d0
JangDongBin <jdb9515@naver.com>
2022-02-12 16:34:24 +0900

    Update BoardController.java

--- src/main/java/com/sample/sample/board/BoardController.java
+++ src/main/java/com/sample/sample/board/BoardController.java
@@ -1,42 +1,64 @@
 package com.sample.sample.board;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.Errors;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
-@RequestMapping("/")
+@RequiredArgsConstructor
+@RequestMapping("/test")
 public class BoardController {
-    
 
+    private final BoardService boardService;
+    private final BoardFormValidator boardFormValidator;
+
+    @InitBinder("boardForm")
+    public void InitBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(boardFormValidator);
+    }
+
+    // 용훈
     @GetMapping("/list")
-    public String HomeList(){
-        return "";
+    public String HomeList() {
+        return "test";
     }
 
-    @GetMapping("/addboard")
-    public String AddBoard(){
-        return "";
+    // 게시글 작성 및 수정 //동빈
+    @GetMapping("/board-add")
+    public String Add_board(Model model, @RequestParam(required = false) Long id) {
+        boardService.detailProcess(model, id);
+        return "board/boardAdd";
     }
 
-    //테스트
-    //테스트
-    
+    @PostMapping("/board-add")
+    public String Post_AddBoard(@Valid BoardForm boardForm, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            return "board/boardAdd";
+        }
+
+        Board newbBoard = boardService.updateProcess(boardForm);
+        return "redirect:/test/detail-board?id=" + newbBoard.getId();
+    }
 
     @GetMapping("/detail-board")
-    public String DetailBoard(){
+    public String DetailBoard(Model model, @RequestParam(required = false) Long id) {
+        boardService.detailProcess(model, id);
         return "board/boardDetail";
     }
 
-    @GetMapping("/pwPopup")
-    public String PasswordPopup(){
-        return "board/boardInsertPw";
-    }
-
-    @PostMapping("/pwPopup")
-    public String PasswordPopupPost(){
-        return "board/boardInsertPw";
+    @GetMapping("/delete")
+    public String Delete_board(Model model, @RequestParam Long id) {
+        return "";
     }
 
 }
