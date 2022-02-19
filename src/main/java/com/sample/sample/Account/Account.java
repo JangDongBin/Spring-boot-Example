8d50efaf9a63cdd67178d10bd2ef5da4ea1c9978
JangDongBin <jdb9515@naver.com>
2022-02-20 03:44:05 +0900

    account table

---
+++ src/main/java/com/sample/sample/Account/Account.java
@@ -0,0 +1,49 @@
+package com.sample.sample.Account;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.Data;
+
+@Data
+@Entity(name = "ACCOUNT")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String userid;
+
+    @Column(nullable = false, length = 30)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false, length = 30)
+    private String tel;
+
+    @Column(nullable = false, length = 30)
+    private String email;
+
+    @Column
+    private String EmailToken;
+
+    @Column(columnDefinition = "boolean default false")
+    private Boolean EmailTokenCheck;
+
+    @CreationTimestamp
+    private LocalDateTime creationTimestamp;
+
+    @UpdateTimestamp
+    private LocalDateTime updateTimestamp; 
+}
