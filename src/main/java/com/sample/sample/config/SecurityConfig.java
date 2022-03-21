08ffb9822f4787267ae0145e632eb3ab422ac66e
SuperYoplait <60890985+SuperYoplait@users.noreply.github.com>
2022-03-21 19:53:10 +0900

    초기 세팅 및 시큐리티 주석처리

--- src/main/java/com/sample/sample/config/SecurityConfig.java
+++ src/main/java/com/sample/sample/config/SecurityConfig.java
@@ -1,8 +1,13 @@
 package com.sample.sample.config;
 
+import java.nio.file.attribute.UserDefinedFileAttributeView;
+
+import javax.sql.DataSource;
+
 import com.sample.sample.account.AccountService;
 import com.sample.sample.account.UserAccount;
 
+import org.springframework.boot.autoconfigure.batch.BatchProperties.Jdbc;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,13 +15,22 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
-
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService AccountService;
+    private final DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -32,6 +46,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/");
 
+
+        http.rememberMe()
+            .userDetailsService(AccountService)
+            .tokenRepository(tokenRepository());
+
+    }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepositoryImpl = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepositoryImpl.setDataSource(dataSource);
+
+        return jdbcTokenRepositoryImpl;
     }
 
     @Override
